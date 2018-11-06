b907da92b55a9144892a30a8d44cd48bc47cf23b
Aasta Lin <aastal@nvidia.com>
2018-11-06 16:37:15 +0800

    fix dependencies issue on Xavier

--- script/install_opencv3.4.0_Xavier.sh
+++ script/install_opencv3.4.0_Xavier.sh
@@ -18,10 +18,9 @@ user="nvidia"
 passwd="nvidia"
 
 echo "** Install requirement"
-sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y build-essential cmake git pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y python2.7-dev
-sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libdc1394-22-dev
 sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
 sudo apt-get update
