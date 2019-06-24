276306e052aba5ff131d6e785960abbb187bacde
aastal <aastal@nvidia.com>
2019-06-24 14:56:10 +0800

    fix bug

--- script/install_opencv4.0.0_Nano.sh
+++ script/install_opencv4.0.0_Nano.sh
@@ -44,7 +44,6 @@ cd release/
 cmake -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.0.0/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j3
 sudo make install
-sudo apt-get install -y python-opencv python3-opencv
 
 echo "** Install opencv-4.0.0 successfully"
 echo "** Bye :)"
