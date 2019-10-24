d78f5930c3ff9c7a42b2fdf5940353c4f6dfe138
aastal <aastal@nvidia.com>
2019-10-24 17:04:11 +0800

    Update OpenCV script for 4.1.1 cross different Jetson platfom

--- script/install_opencv4.1.1_Jetson.sh
+++ script/install_opencv4.1.1_Jetson.sh
@@ -17,7 +17,7 @@ folder="$1"
 user="nvidia"
 passwd="nvidia"
 
-echo "** Remove OpenCV3.3 first"
+echo "** Remove other OpenCV first"
 sudo sudo apt-get purge *libopencv*
 
 echo "** Install requirement"
@@ -25,25 +25,30 @@ sudo apt-get update
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
-sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
 sudo apt-get update
 
-echo "** Download opencv-4.0.0"
+echo "** Download opencv-4.1.1"
 cd $folder
-curl -L https://github.com/opencv/opencv/archive/4.0.0.zip -o opencv-4.0.0.zip
-curl -L https://github.com/opencv/opencv_contrib/archive/4.0.0.zip -o opencv_contrib-4.0.0.zip
-unzip opencv-4.0.0.zip 
-unzip opencv_contrib-4.0.0.zip 
-cd opencv-4.0.0/
+curl -L https://github.com/opencv/opencv/archive/4.1.1.zip -o opencv-4.1.1.zip
+curl -L https://github.com/opencv/opencv_contrib/archive/4.1.1.zip -o opencv_contrib-4.1.1.zip
+unzip opencv-4.1.1.zip
+unzip opencv_contrib-4.1.1.zip
+cd opencv-4.1.1/
+
+echo "** Apply patch"
+sed -i 's/include <Eigen\/Core>/include <eigen3\/Eigen\/Core>/g' modules/core/include/opencv2/core/private.hpp
 
 echo "** Building..."
 mkdir release
 cd release/
-cmake -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.0.0/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3,6.2,7.2" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.1.1/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j3
 sudo make install
+echo 'export PYTHONPATH=$PYTHONPATH:'$PWD'/python_loader/' >> ~/.bashrc
+source ~/.bashrc
 
-echo "** Install opencv-4.0.0 successfully"
+echo "** Install opencv-4.1.1 successfully"
 echo "** Bye :)"
