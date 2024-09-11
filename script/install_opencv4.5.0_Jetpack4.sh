0d92d1e8db8b46e0fa615a796f75f63ecc12d099
Your Name <you@example.com>
2024-09-11 14:35:05 +0800

    update opencv 4.9 script

--- script/install_opencv4.5.0_Jetpack4.sh
+++ script/install_opencv4.5.0_Jetpack4.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 #
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA Corporation and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA Corporation is strictly prohibited.
-#
 
 version="4.5.0"
 folder="workspace"
